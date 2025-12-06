ca908513893c8ba03d813872c662f54567c883fa
Igbaria <al3arbe1@gmail.com>
2025-12-06 07:42:31 +0200

    feat(ios): Implement contact checker using canOpenURL

--- ios/Classes/AppContactCheckerPlugin.swift
+++ ios/Classes/AppContactCheckerPlugin.swift
@@ -9,11 +9,31 @@ public class AppContactCheckerPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    // The 'phone' argument is received but not used on iOS, as we can only check
+    // if the application is installed, not if a specific contact exists.
+    // This is a platform limitation. We keep the argument for API consistency with Android.
+    guard call.arguments is [String: Any] else {
+        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Arguments are not a map", details: nil))
+        return
+    }
+
+    let urlScheme: String
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "isOnWhatsApp":
+      urlScheme = "whatsapp://"
+    case "isOnWhatsAppBusiness":
+      urlScheme = "whatsappbusiness://"
+    case "isOnTelegram":
+      urlScheme = "tg://"
     default:
       result(FlutterMethodNotImplemented)
+      return
+    }
+
+    if let url = URL(string: urlScheme) {
+        result(UIApplication.shared.canOpenURL(url))
+    } else {
+        result(false) // Should not happen with hardcoded schemes
     }
   }
 }
